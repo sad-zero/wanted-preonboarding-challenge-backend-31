c9e2b11ae6659c3065cc566c832b2edf7efd615b
sad-zero <zeroro.yun@gmail.com>
2025-05-04 10:43:58 +0900

    chore: setup project

---
+++ src/main/java/kr/co/wanted/backend31/Backend31Application.java
@@ -0,0 +1,13 @@
+package kr.co.wanted.backend31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend31Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/wanted/backend31/Backend31ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wanted.backend31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
